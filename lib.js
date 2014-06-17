97191a46e2b8c50836ecb0aeff065e00793a9612
Carter B. Smith <>
2014-06-17 10:14:48 -0500

    now deliveryReport returns object with smsid(str), send(bool), and message(str)

--- lib.js
+++ lib.js
@@ -1,24 +1,35 @@
 var http = require("http");
 
+//--------------
+// takes: smsid and shortode
+// returns: object with data-[smsid(str), send(bool), message(str)]
+//--------------
 var deliveryReport = function(smsid, shortcode, callback){
-    http.get("http://mosms.sumotext.com/secure/sumoReport.aspx?smsid=" + smsid + "&shortcode=" + shortcode, function (http_res) {
-    // initialize the container for our data
-    var data = "";
+    
+    http.get("http://mosms.sumotext.com/secure/sumoReport.aspx?smsid=" 
+                + smsid + "&shortcode=" + shortcode, 
+        function (http_res) {
+            // initialize the container for our data
+            var data = "";
 
-    // this event fires many times, each time collecting another piece of the response
-    http_res.on("data", function (chunk) {
-        // append this chunk to our growing `data` var
-        data += chunk;
-    });
+            // this event fires many times, each time collecting another piece of the response
+            http_res.on("data", function (chunk) {
+                // append this chunk to our growing `data` var
+                data += chunk;
+            });
 
-    // this event fires *one* time, after all the `data` events/chunks have been gathered
-    http_res.on("end", function () {
-        // you can use res.send instead of console.log to output via express
-        data = data.split('\n')[0];
+            // this event fires *one* time, after all the `data` events/chunks have been gathered
+            http_res.on("end", function () {
+                // you can use res.send instead of console.log to output via express
+                data = data.split('\n')[0];
+                var lineSplit = data.split(':');
+                var newData = { smsid :     lineSplit[0],
+                                sent :      lineSplit[1],
+                                message :   lineSplit[2] };
 
-        callback(data);
-    });
-});
+                callback(newData);
+            });
+        });
 }
 
 module.exports.deliveryReport = deliveryReport;
--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 var sumo = require('./lib.js');
 
-sumo.deliveryReport('7A2D2AF7-6851-4D22-BD41-BD8EE94C061E', 
-					'74700', 
-	function(data){
-		console.log(data);
+var smsid = '7A2D2AF7-6851-4D22-BD41-BD8EE94C061E';
+var shortcode = '74700';
+
+sumo.deliveryReport(smsid, shortcode, function(data){
+	console.log(data);
 });
 
