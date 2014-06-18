75e90679b2d3632199964e9c3adf2b4c2df7e6f5
Carter B. Smith <>
2014-06-17 21:16:36 -0500

    works for delivreport

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
 var sumo = require('./lib');
 
-var smsid = '7A2D2AF7-6851-4D22-BD41-BD8EE94C061E';
-var shortcode = '74700';
-/*
-sumo.deliveryReport(smsid, shortcode, function(data){
+var _smsid = '7A2D2AF7-6851-4D22-BD41-BD8EE94C061E';
+var _shortcode = '74700';
+
+sumo.deliveryReport({
+	smsid : 	_smsid, 
+	shortcode : _shortcode
+}, function(data){
 	console.log(data);
 });
-
+/*
 sumo.sendMt('5012589948', 'CINGULARUS', '74700', 'CSAPI', 'hey', function(data){
 	console.log(data);
 });*/
 
-sumo.carrierCodeLookup({mobile: '5014139908', shortcode:'74700'}, function(data){
+sumo.carrierCodeLookup({
+	mobile : 	'5014139908', 
+	shortcode : '74700'
+}, function(data){
 	console.log(data);
 });
