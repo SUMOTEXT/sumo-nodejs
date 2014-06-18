0fc97e044e9e7e39278a941e2cf2f241c5172222
Carter B. Smith <>
2014-06-17 21:04:12 -0500

    looks up carrier code

--- main.js
+++ main.js
@@ -2,11 +2,15 @@ var sumo = require('./lib');
 
 var smsid = '7A2D2AF7-6851-4D22-BD41-BD8EE94C061E';
 var shortcode = '74700';
-
+/*
 sumo.deliveryReport(smsid, shortcode, function(data){
 	console.log(data);
 });
 
 sumo.sendMt('5012589948', 'CINGULARUS', '74700', 'CSAPI', 'hey', function(data){
+	console.log(data);
+});*/
+
+sumo.carrierCodeLookup('5014139908', '74700', function(data){
 	console.log(data);
 });
