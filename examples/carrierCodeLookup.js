0f2338cf850f5657d99705410954c860737d95e5
Carter B. Smith <>
2014-06-20 14:33:21 -0500

    asdf

---
+++ examples/carrierCodeLookup.js
@@ -0,0 +1,16 @@
+sumo = require('../lib');
+
+sumo.carrierCodeLookup({
+	mobile : 	'{mobile}', 
+	shortcode : '{shortcode}'
+}, function(err, data){
+	
+	if (err){
+		console.log(err)
+	} else {
+		console.log(data);
+	}
+}); 
+
+//response type: string
+//response format: {carrier_code}
