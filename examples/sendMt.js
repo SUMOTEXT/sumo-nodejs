2d2deef2cbe6327578dd45b2b9f4968e85a69618
Carter B. Smith <>
2014-06-19 22:57:24 -0500

    added examples

---
+++ examples/sendMt.js
@@ -0,0 +1,16 @@
+sumo = require('../lib');
+
+sumo.sendMt({
+	mobile: '{mobile}', 
+	carrier: '{carrier_code}', 
+	shortcode: '{shortcode}',
+	keyword: '{keyword}',
+	msg: 'hello there'
+}, function(err, data){
+	if (err){
+		console.log(err); 
+	} else {
+		console.log(data);
+	}
+});
+//response params: mobile, smsid
